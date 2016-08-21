9af9aff3127bae4b69c34e6b51e3a61cb2a19bb9
柴田敬介 <skyouseif8>
2016-08-21 13:35:45 +0900

    Initial Commit

---
+++ stampmaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stampmaker
+//
+//  Created by 柴田　樹希 on 2016/08/21.
+//  Copyright © 2016年 柴田　樹希. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
